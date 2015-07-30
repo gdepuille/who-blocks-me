2a155cce81da508aa6d6afdaf7480de46fdfa98a
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2015-07-31 00:46:56 +0200

    Design de base.
    Signed-off-by: Gregory DEPUILLE <gregory.depuille@sogelink.fr>

--- wbm-front/lib/views/login_page.dart
+++ wbm-front/lib/views/login_page.dart
@@ -3,21 +3,36 @@
 
 import 'package:polymer/polymer.dart';
 
-import 'package:paper_elements/paper_button.dart';
 import 'package:paper_elements/paper_input.dart';
+import 'package:core_elements/core_input.dart';
 
 import '../delegate/auth_delegate.dart';
 
 @CustomTag('login-page')
 class LoginPage extends PolymerElement {
 
+  PaperInput _tiUsername;
+  CoreInput _tiPassword;
+
   LoginPage.created() : super.created();
 
+  attached() {
+    _tiUsername = $['tiUsername'] as PaperInput;
+    _tiPassword = $['tiPassword'] as CoreInput;
+  }
+
   void loginHandler() {
-    String username = ($['tiUsername'] as PaperInput).value;
-    String password = ($['tiPassword'] as PaperInput).value;
+    String username = _tiUsername.committedValue;
+    String password = _tiPassword.committedValue;
 
     AuthDelegate authDelegate = new AuthDelegate();
     authDelegate.login(username, password);
+
+    _resetFields();
+  }
+
+  _resetFields() {
+    _tiPassword.value = null;
+    _tiUsername.value = null;
   }
 }
